228c7eea365c7452ba3c8aa6b0633f039d10c2c6
Olafur Arason <olafura@olafura.com>
2014-05-04 14:46:46 -0400

    Save battery and resources by handling events instead of polling for wifi access points

--- wifi.py
+++ wifi.py
@@ -1,5 +1,5 @@
-#import gobject
-#from dbus.mainloop.glib import DBusGMainLoop
+import gobject
+from dbus.mainloop.glib import DBusGMainLoop
 import dbus
 import json
 from mapping import wifi_states
@@ -14,11 +14,10 @@ couch = couchdb.Server()
 couch.resource.credentials = (username,password)
 db = couch["wifi"]
 
-#DBusGMainLoop(set_as_default=True)
-#loop = gobject.MainLoop()
+DBusGMainLoop(set_as_default=True)
+loop = gobject.MainLoop()
 import NetworkManager
 bus = dbus.SystemBus()
-nm = bus.get_object('org.freedesktop.NetworkManager', '/org/freedesktop/NetworkManager')
 
 currentdev = None
 
@@ -27,6 +26,24 @@ for device in NetworkManager.NetworkManager.GetDevices():
         currentdev = device.SpecificDevice()
         break
 
+def getAP(point, active_op):
+        try:
+            doc = db[point.Ssid]
+        except couchdb.http.ResourceNotFound:
+            doc = {}
+        doc["_id"] = point.Ssid
+        is_active = point.object_path == active_op
+        n = datetime.datetime.now()
+        #I'm using unix time bacause it makes it easy to do math
+        unix_time = time.mktime(n.timetuple())
+        if "time" in doc:
+            doc["last_time"] = doc["time"]
+        doc["time"] = unix_time
+        doc["is_active"] = is_active
+        doc["strength"] = int(point.proxy.Get(point.interface_name, "Strength",dbus_interface="org.freedesktop.DBus.Properties"))
+        return doc
+
+
 def checkAccessPoints():
     if not currentdev == None:
         try:
@@ -34,36 +51,24 @@ def checkAccessPoints():
             access_points = currentdev.GetAccessPoints()
         except dbus.exceptions.DBusException:
             return
+        active_op = active.object_path
         for point in access_points:
-            try:
-                doc = db[point.Ssid]
-            except couchdb.http.ResourceNotFound:
-                doc = {}
-            doc["_id"] = point.Ssid
-            is_active = point.object_path == active.object_path
-            n = datetime.datetime.now()
-            #I'm using unix time bacause it makes it easy to do math
-            unix_time = time.mktime(n.timetuple())
-            if "time" in doc:
-                doc["last_time"] = doc["time"]
-            doc["time"] = unix_time
-            doc["is_active"] = is_active
-            doc["strength"] = int(point.proxy.Get(point.interface_name, "Strength",dbus_interface="org.freedesktop.DBus.Properties"))
+            doc = getAP(point, active_op)
             db.save(doc)
     #print "end"
 
-while True:
-    #print "begin"
-    checkAccessPoints()
-    time.sleep(5)
+def handle_apadded(ap):
+    print("new_ap")
+    active = currentdev.ActiveAccessPoint
+    active_op = active.object_path
+    doc = getAP(ap, active_op)
+    db.save(doc)
+    #print ap
 
-#I can't seem to get the interval loop working even the most simple
-#example working on the gobject mainloop, there is no time to explore this
-#further before I hand it in
+def handle_apremove(ap):
+    print("delete_ap")
 
-#def handle_notification(state):
-#    print wifi_states[int(state)]
+currentdev.connect_to_signal("AccessPointAdded", handle_apadded)
+currentdev.connect_to_signal("AccessPointRemoved", handle_apremove)
 
-#nm.connect_to_signal("StateChanged", handle_notification)
-
-#loop.run()
+loop.run()
